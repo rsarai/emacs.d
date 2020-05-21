37ee2e025d2bc69b25acd59002f107325a05450d
rsarai <rebecasaraiaguiar@gmail.com>
2020-05-21 20:25:26 -0300

    Updates daily notes name

--- init.el
+++ init.el
@@ -68,19 +68,19 @@
 
 (use-package org-roam
       :load-path "~/.emacs.d/elisp/"
-      :hook 
+      :hook
       (after-init . org-roam-mode)
       :custom
       (org-roam-directory "/home/sarai/github-projects/second-brain/brain")
       :custom-face
       (org-roam-link ((t (:inherit org-link :foreground "#C991E1"))))
-      :straight (:host github :repo "jethrokuan/org-roam" :branch "develop")
+      :straight (:host github :repo "jethrokuan/org-roam" :branch "master")
       :bind (:map org-roam-mode-map
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
                ("C-c n g" . org-roam-show-graph)
                ("C-c n i" . org-roam-insert))))
-      
+
 (setq org-roam-filename-noconfirm nil)
 
 (use-package deft
@@ -98,7 +98,7 @@
   ("C-c n j" . org-journal-new-entry)
   :custom
   (org-journal-date-prefix "#+TITLE: ")
-  (org-journal-file-format "%Y-%m-%d.org")
+  (org-journal-file-format "private%Y-%m-%d.org")
   (org-journal-dir "/home/sarai/github-projects/second-brain/brain")
   (org-journal-date-format "%B %d, %Y")
   :init (setq org-log-done 'time))
@@ -122,16 +122,38 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tsdh-dark)))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(deft-default-extension "org" t)
  '(deft-directory "/home/sarai/github-projects/second-brain/brain")
  '(deft-recursive t)
  '(deft-use-filter-string-for-filename t)
  '(mathpix-app-id "app-id" t)
- '(mathpix-app-key "app-key" t))
+ '(mathpix-app-key "app-key" t)
+ '(org-catch-invisible-edits (quote show))
+ '(org-journal-date-format "%B %d, %Y")
+ '(org-journal-date-prefix "#+TITLE: ")
+ '(org-journal-dir "/home/sarai/github-projects/second-brain/brain")
+ '(org-journal-file-format "private%Y-%m-%d.org")
+ '(org-preview-latex-image-directory "/tmp/ltximg/")
+ '(org-return-follows-link t)
+ '(org-roam-directory "/home/sarai/github-projects/second-brain/brain")
+ '(org-src-window-setup (quote current-window))
+ '(org-use-speed-commands t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:weight bold :height 1.5))))
+ '(org-done ((t (:strike-through t :weight bold))))
+ '(org-headline-done ((t (:strike-through t))))
+ '(org-image-actual-width (/ (display-pixel-width) 2))
+ '(org-level-1 ((t (:height 1.3 :weight bold))))
+ '(org-level-2 ((t (:height 1.2 :weight bold))))
+ '(org-level-3 ((t (:height 1.1 :weight bold))))
+ '(org-roam-link ((t (:inherit org-link :foreground "#C991E1"))))
+ '(variable-pitch ((t (:family "Libre Baskerville")))))
